5a82570a53e309a9b060b22ed73c0a31db0ed89e
Yared (Nur Solutions) <nursolutionsplc@gmail.com>
2026-07-21 19:35:16 +0300

    feat: sample update to page.tsx

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -5,7 +5,8 @@ import { DEFAULT_PORT } from "@rona/config";
 const Page = () => {
   return (
     <div>
-      <Input />
+      <h1>Welcome to Rona ERP Solutions!</h1>
+      <Input placeholder="Input your name" />
       <Button>Button {DEFAULT_PORT}</Button>
     </div>
   );
