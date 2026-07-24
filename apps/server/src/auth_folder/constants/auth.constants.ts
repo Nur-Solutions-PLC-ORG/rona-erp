8eea2af8a7bd6cce1fc4ccb0df434fcc6cff8e6d
Oliprg3 <136181155+Oliprg3@users.noreply.github.com>
2026-07-24 09:39:17 +0300

    feat: updated auth.constants.ts

--- apps/server/src/auth_folder/constants/auth.constants.ts
+++ apps/server/src/auth_folder/constants/auth.constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET || 'cc00dbda198edbf6c6f13ea6bbc30b2311a0d012787419f85223c6e34abebdca',
+  secret: process.env.JWT_SECRET || 'key',
 };
