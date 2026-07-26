1569b1aee397959a37b50d6d9142c8b28509781f
Oliprg3 <136181155+Oliprg3@users.noreply.github.com>
2026-07-26 20:19:27 +0300

    Update google-login.dto.ts

--- apps/server/src/auth/dto/google-login.dto.ts
+++ apps/server/src/auth/dto/google-login.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 export class GoogleLoginDto {
   @IsNotEmpty()
   @IsString()
-  @Length(1, 2048, { message: 'idToken must be between 1 and 2048 characters' })
+  @Length(1, 2048, { message: 'incorrect id token' })
   @Matches(/^[A-Za-z0-9\-_]+\.([A-Za-z0-9\-_]+)\.([A-Za-z0-9\-_]+)$/, {
     message: 'idToken must be a valid JWT format',
   })
