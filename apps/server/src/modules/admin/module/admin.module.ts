820315aadc8d785e833b9b03ca6f7b7f18125fea
Oliprg3 <oliyadabenet@gmail.com>
2026-08-06 22:33:48 +0300

    fix: import AuthModule in AdminModule and rebuild packages

--- apps/server/src/modules/admin/module/admin.module.ts
+++ apps/server/src/modules/admin/module/admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from '../controller/admin.controller';
 import { AdminService } from '../service/admin.service';
+import { AuthModule } from '../../auth/module/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
