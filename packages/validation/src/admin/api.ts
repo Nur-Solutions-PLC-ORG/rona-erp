c8dbd6524d8996c9514569eaea1b90dfd3c1cc4e
Yared Betemariam <yaredbelaybtm@gmail.com>
2026-08-22 13:06:55 +0300

    feat: restructured validation packages 1

---
+++ packages/validation/src/admin/api.ts
@@ -0,0 +1,28 @@
+import { POSITIONS_LIST, USER_STATUS_LIST } from "@rona/config/auth";
+import { paginationSearchParamsSchema } from "../global/api.js";
+import z from "zod";
+import {
+  EMPLOYEE_STATUS_LIST,
+  ORGANIZATION_STATUS_LIST,
+} from "@rona/config/admin";
+
+export const userListSearchParamsSchema = paginationSearchParamsSchema.extend({
+  status: z.enum(USER_STATUS_LIST).optional(),
+  position: z.enum(POSITIONS_LIST).optional(),
+});
+
+export const organizationListSearchParamsSchema =
+  paginationSearchParamsSchema.extend({
+    status: z.enum(ORGANIZATION_STATUS_LIST).optional(),
+  });
+
+export const employeeListSearchParamsSchema =
+  paginationSearchParamsSchema.extend({
+    status: z.enum(EMPLOYEE_STATUS_LIST).optional(),
+  });
+
+export const departmentListSearchParamsSchema = paginationSearchParamsSchema;
+export const branchListSearchParamsSchema = paginationSearchParamsSchema;
+export const configsListSearchParamsSchema = paginationSearchParamsSchema;
+export const organizationSettingsListSearchParamsSchema =
+  paginationSearchParamsSchema;
